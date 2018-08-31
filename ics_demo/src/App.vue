3d8a04480d67fc4b392236b5de464c1a8b7c923f
zhouxiaohang <zhouxiaohang@autohome.com.cn>
2018-08-31 16:41:45 +0800

    css

--- ics_demo/src/App.vue
+++ ics_demo/src/App.vue
@@ -9,6 +9,10 @@ export default {
   name: 'App'
 }
 </script>
-
 <style>
+/*引入公共样式*/
+@import './../static/css/base.min.css';
+@import './../static/css/common.css';
+@import './../static/css/common_new.css';
+@import './../static/css/blue_skin.min.css';
 </style>
